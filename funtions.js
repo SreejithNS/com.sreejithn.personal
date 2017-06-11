c9df06c65725983b2def8a433f20a73f12402ff0
Sreejith N <nssreejith229@gmail.com>
2017-06-11 20:21:28 +0530

    Add files via upload

--- funtions.js
+++ funtions.js
@@ -1,16 +1,11 @@
 
-$(document).on("mobileinit","#home",function(){
-	 $( document ).on( "swipeleft", "#home", function( e ) {
+$(document).on("pagecreate","#home",function(){
+	 $( document ).on( "swipe", "#home", function( e ) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).
         // We do this by checking the data that the framework stores on the page element (panel: open).
-        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
-            if ( e.type === "swipeleft"  ) {
+
                 $( "#panel" ).panel( "open" );
-            } else if ( e.type === "swiperight" ) {
-                $( "#panel" ).panel( "open" );
-            }
-        }
     });
 
 });
@@ -24,14 +19,41 @@ $(document).on( "pagecreate", "#index", function() {
 	var pass = document.getElementById("pass").value;
 	var password = "demo";
 	if (pass == password ) {
+		$.mobile.changePage( "#home", { transition: "slidefade"});
 		$("#pass").val("");
-		$.mobile.changePage( "#home", { transition: "slideup"});
+		return false;
 	} else {
 		$.mobile.changePage( "#login-error", { transition: "pop",overlayTheme: "b"});
 		$("#pass").val("");
 	};
+
 	});
 });
 
+function login(){
+	var pass = document.getElementById("pass").value;
+	var password = "demo";
+	if (pass == password ) {
+		$("#pass").val("");
+		$.mobile.changePage( "#home", { transition: "slidefade"});
+	} else {
+		$.mobile.changePage( "#login-error", { transition: "pop",overlayTheme: "b"});
+		$("#pass").val("");
+	}
+}
 
+ function onLoad()
+            {
+                  document.addEventListener("deviceready", onDeviceReady, true);
+            }
 
+            function exitFromApp()
+             {
+				 if (navigator.app) {
+                        navigator.app.exitApp();
+                    }
+                    else if (navigator.device) {
+                        navigator.device.exitApp();
+
+                    }
+             }
